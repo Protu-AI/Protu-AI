1669c98a99144b78b2b05eea684aba034998cb38
hazem <hazemelakbawy62@gmail.com>
2025-03-13 16:00:01 +0200

    feat: Add RabbitMQ retry mechanism and convert properties classes to records
    - Implement retry mechanism for handling RabbitMQ message failures
    - Convert property classes to records for improved immutability.
    - Update application-local-template.yaml files in user & notification services
    
    References:
    - https://medium.com/@saulosaires/implementing-rabbitmq-retries-with-spring-boot-7fe58c14cb12

--- Protu-Backend/notification-service/src/main/java/org/protu/notificationservice/dto/RabbitMQMessage.java
+++ Protu-Backend/notification-service/src/main/java/org/protu/notificationservice/dto/RabbitMQMessage.java
@@ -4,7 +4,10 @@ import java.sql.Timestamp;
 
 public record RabbitMQMessage(String messageId, String to, String from, Template template, MetaData metaData) {
   public record Template(String id, EmailData data) {
-    public record EmailData(String username, String otp, int otpTtl) {}
+    public record EmailData(String username, String otp, int otpTtl) {
+    }
   }
-  public record MetaData(String service, Timestamp timestamp) {}
+
+  public record MetaData(String service, Timestamp timestamp) {
+  }
 }
