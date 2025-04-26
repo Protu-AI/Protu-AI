fbbf5df0d2d6c5c5840836f00a8452741357ec9e
hazem <hazemelakbawy62@gmail.com>
2025-04-26 18:27:40 +0300

    refactor(content): replace ID path params with resource names
    - Update endpoints to use resource names instead of IDs in path parameters
    - Refactor ProgressService code to improve JPA performance.
    - Add @Transactional annotations to methods that modify data.
    - Add error handling for UserNotFoundException

--- Protu-Backend/content-service/src/main/java/org/protu/contentservice/common/config/SecurityConfig.java
+++ Protu-Backend/content-service/src/main/java/org/protu/contentservice/common/config/SecurityConfig.java
@@ -12,14 +12,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
-import java.time.Duration;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
